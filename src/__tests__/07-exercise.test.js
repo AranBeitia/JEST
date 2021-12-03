af316545f3e91c53c5be40997c14b3fea92e92c2
AranBeitia <arantza.beitia@gmail.com>
2021-12-03 18:46:03 +0100

    tests

--- src/__tests__/07-exercise.test.js
+++ src/__tests__/07-exercise.test.js
@@ -1,41 +1,36 @@
-import axios from "axios";
-import { getUserData } from "../utils/modules";
+import axios from 'axios'
+import { getUserData } from '../utils/modules'
 
 // 1. mock the axios module
-jest.mock("axios");
 
 /**
  * Write the assertions using the most appropriate matcher
  */
-describe("07-exercises", () => {
-  test("getUserData returns the user data", async () => {
-    const BASE_URL = "https://www.api.com/users/1";
+describe('07-exercises', () => {
+	test('getUserData returns the user data', async () => {
+		const BASE_URL = 'https://www.api.com/users/1'
 
-    const users = {
-      data: [
-        { firstName: "Alex", lastName: "Marks" },
-        { firstName: "Ana", lastName: "Marín" },
-        { firstName: "Daniel", lastName: "Marks" },
-      ],
-    };
+		const users = {
+			data: [
+				{ firstName: 'Alex', lastName: 'Marks' },
+				{ firstName: 'Ana', lastName: 'Marín' },
+				{ firstName: 'Daniel', lastName: 'Marks' },
+			],
+		}
 
-    // You should make 2 assertions in this test, see bellow
-    expect.assertions(2);
+		// You should make 2 assertions in this test, see bellow
+		expect.assertions(2)
 
-    // 2. set a mock resolved value to the axios.get method
-    //    and pass it the `users` variable
-    axios.get.mockResolvedValue(users);
+		// 2. set a mock resolved value to the axios.get method
+		//    and pass it the `users` variable
 
-    // 3. use await with the getUsers function and pass it as an argument
-    //    the `BASE_URL` variable and then store the returned data
-    //    in a variable `result`
-    const result = await getUserData(BASE_URL);
+		// 3. use await with the getUsers function and pass it as an argument
+		//    the `BASE_URL` variable and then store the returned data
+		//    in a variable `result`
 
-    // 4. make an assertion that the `result` is equal to the `users.data` variable
-    expect(result).toEqual(users.data);
+		// 4. make an assertion that the `result` is equal to the `users.data` variable
 
-    // 5. make an assertion that the axios.get method has been
-    //    called with the `BASE_URL` variable
-    expect(axios.get).toHaveBeenCalledWith(BASE_URL);
-  });
-});
+		// 5. make an assertion that the axios.get method has been
+		//    called with the `BASE_URL` variable
+	})
+})
--- src/__tests__/09-exercise.test.js
+++ src/__tests__/09-exercise.test.js
@@ -1,66 +1,56 @@
-import { calculator, increment } from "../utils/math";
+import { calculator, increment } from '../utils/math'
 
 /**
  * Write the assertions using the most appropriate matcher
  */
-describe("09-exercises", () => {
-  let addSpy = null;
+describe('09-exercises', () => {
+	let addSpy = null
 
-  beforeAll(() => {
-    // 1. Spy on the 'add' method of the calculator object
-    //    and save the spy in the `addSpy` variable
-    addSpy = jest.spyOn(calculator, "add");
-  });
+	beforeAll(() => {
+		// 1. Spy on the 'add' method of the calculator object
+		//    and save the spy in the `addSpy` variable
+	})
 
-  // 2. Clear the mock calls to the addSpy so that the tests work
-  beforeEach(() => addSpy.mockClear());
+	// 2. Clear the mock calls to the addSpy so that the tests work
+	beforeEach(() => {})
 
-  // 3. Restore the initial implementation of the addSpy using .mockRestore()
-  afterAll(() => addSpy.mockRestore());
+	// 3. Restore the initial implementation of the addSpy using .mockRestore()
 
-  test("increment returns 10 for 7 + 3", () => {
-    let result = null;
+	test('increment returns 10 for 7 + 3', () => {
+		let result = null
 
-    /**
-     * 4. Execute the `increment` function by passing the arguments: (7, 3, calculator)
-     *    and store the result in the `result` variable
-     */
-    result = increment(7, 3, calculator);
+		/**
+		 * 4. Execute the `increment` function by passing the arguments: (7, 3, calculator)
+		 *    and store the result in the `result` variable
+		 */
 
-    // You should make 3 assertions in this test, see bellow
-    expect.assertions(3);
+		// You should make 3 assertions in this test, see bellow
+		expect.assertions(3)
 
-    // 5. Make an assertion that the `result` is 10
-    expect(result).toBe(10);
+		// 5. Make an assertion that the `result` is 10
 
-    // 6. Make an assertion that the addSpy has been called 1 time
-    expect(addSpy).toBeCalledTimes(1);
+		// 6. Make an assertion that the addSpy has been called 1 time
 
-    // 7. Make an assertion that the addSpy has been called with the
-    //    numbers 7 and 3
-    expect(addSpy).toHaveBeenCalledWith(7, 3);
-  });
+		// 7. Make an assertion that the addSpy has been called with the
+		//    numbers 7 and 3
+	})
 
-  test("increment returns 15 for 10 + 5", () => {
-    let result = null;
+	test('increment returns 15 for 10 + 5', () => {
+		let result = null
 
-    /**
-     * 8. Execute the `increment` function by passing the arguments: (10, 5, calculator)
-     *    and store the result in the `result` variable
-     */
-    result = increment(10, 5, calculator);
+		/**
+		 * 8. Execute the `increment` function by passing the arguments: (10, 5, calculator)
+		 *    and store the result in the `result` variable
+		 */
 
-    // You should make 3 assertions in this test, see bellow
-    expect.assertions(3);
+		// You should make 3 assertions in this test, see bellow
+		expect.assertions(3)
 
-    // 9. Make an assertion that the `result` is 15
-    expect(result).toBe(15);
+		// 9. Make an assertion that the `result` is 15
 
-    // 10. Make an assertion that the addSpy has been called 1 time
-    expect(addSpy).toBeCalledTimes(1);
+		// 10. Make an assertion that the addSpy has been called 1 time
 
-    // 11. Make an assertion that the addSpy has been called with the
-    //     numbers 10 and 5
-    expect(addSpy).toHaveBeenCalledWith(10, 5);
-  });
-});
+		// 11. Make an assertion that the addSpy has been called with the
+		//     numbers 10 and 5
+	})
+})
