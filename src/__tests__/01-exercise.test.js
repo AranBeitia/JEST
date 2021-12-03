f556dc3b5111497a4d073c1da4272d01a9e05a84
AranBeitia <arantza.beitia@gmail.com>
2021-12-03 11:29:22 +0100

    first exercise

--- src/__tests__/01-exercise.test.js
+++ src/__tests__/01-exercise.test.js
@@ -1,14 +1,14 @@
-import { add } from "../utils/numbers";
+import { add } from '../utils/numbers'
 
 /**
  * Finish the test so that it checks if the result
  * of calling the add function with the arguments of
  * 2 and 4 is 6
  */
-describe("01-exercise", () => {
-  test("add return the sum of the numbers", () => {
-    expect.assertions(1);
-
-    // Finish the test
-  });
-});
+describe('01-exercise', () => {
+	test('add return the sum of the numbers', () => {
+		expect.assertions(1)
+		expect(2 + 4).toBe(6)
+		// Finish the test
+	})
+})
